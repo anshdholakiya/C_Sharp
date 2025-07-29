f15fb2616bd7f4752d0bf956da3d77e045a84105
Ansh Dholakiya <dholakiyaansh1707@gmail.com>
2025-07-29 21:15:45 +0530

    29 july

---
+++ Nirav sir/OOP/basic_oop.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+public class Class1
+{
+	public class Employee()
+	{
+		int empId; // this is knowns as instance variable in the class
+		string empName;
+		double salary;
+
+		public void getData(int empId , string empName , double salary)
+		{
+			this.empId = empId;
+			this.empName = empName;
+			this.salary = salary;
+		}
+
+		public double getSalary()
+		{
+			return this.salary;
+		}
+
+		public void showData()
+		{
+			Console.WriteLine(empId);
+			Console.WriteLine(empName);
+			Console.WriteLine(salary);
+		}
+
+	}
+
+
+	public class Time
+	{
+		int hour;
+		int min;
+		int sec;
+
+		public void setData(int hour, int min, int sec)
+		{
+			this.hour = hour;
+			this.min = min;
+			this.sec = sec;
+		}
+		public void increment()
+		{
+			if(this.sec == 59) {
+				this.sec = 0;
+				if (this.min == 59){
+					this.min = 0;
+					if (this.hour == 24) {
+						this.hour = 1;
+					}
+					else
+					{
+						this.hour++;
+					}
+				}
+				else
+				{
+					this.min++;
+				}
+			}
+			else
+			{
+				this.sec++;
+			}
+		}
+		
+		public void getData()
+		{
+			Console.WriteLine("Time is {0}:{1}:{2}", this.hour, this.min, this.sec);
+		}
+
+	}
+
+	public static void Main(string[] args)
+	{
+		Employee e1 = new Employee();
+		e1.getData(2,"ksihan",10); 
+
+		Employee e2 = new Employee();
+		e2.getData(3,"jay",13200);
+
+		//if (e1.getSalary() > e2.getSalary()) 
+		//	e1.showData();
+		//else
+		//	e2.showData();
+		 
+		//employee.empId = 234; // we can't do it because of encapsulation
+
+		Time time = new Time();
+		time.setData(11, 59, 59);
+		time.increment();
+		time.getData();
+	}
+}
---
+++ Nirav sir/OOP/notes.md
@@ -0,0 +1,43 @@
+# why c# is object oriented programming language
+
+* because it have class and object 
+* it support encapsulation ,abstraction ,polimorphism ,Inheritance
+* method overriding 
+* method overloading
+* c# language is oop based because main method in class 
+
+
+## Encapsulation
+* it wrap the data in in private method . so data is hidden 
+* bind your data together, data hiding members => not accessible from outside by default all members are private
+
+## Abstraction
+* only show necessary information to specific user is called abstraction
+* providing functionalities to the users and hiding interal detail abstradt keyword , interface*
+
+
+## Polimorphism
+* compile time(method overloading) - same method name and different argument  and run time(method overiding) 
+
+
+
+## Inheritance
+* here one calss can inherited in another parent calss for example Animal is parent calss and cow is child class
+* every animal can eat. cow can also eat*
+* had five type
+* single
+* multiple
+* mulit-level
+* hirarchical
+* hybrid
+
+
+# what is class
+* user define data type
+*  states and behaviors
+	* blue print of data
+
+# object 
+* instanse of a class
+
+
